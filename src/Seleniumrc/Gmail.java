9ebc390ab1b0b99c5038e1b6cb421645d2b7dab1
BarkhaSoni <soni.barkha@technocentra.com>
2019-06-11 12:34:57 +0530

    intial commit for selinium code

---
+++ src/Seleniumrc/Gmail.java
@@ -0,0 +1,10 @@
+package Seleniumrc;
+
+public class Gmail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Seleniumrc/GoogleSearch.java
@@ -0,0 +1,12 @@
+package Seleniumrc;
+
+import java.awt.JobAttributes.DefaultSelectionType;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultSelenium selenium=new defaultsele
+	}
+}
+
