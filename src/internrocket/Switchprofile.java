b30b7db0b8cce1b72c2be58db185d36c17524c04
BarkhaSoni <soni.barkha@technocentra.com>
2019-06-24 17:22:27 +0530

    Latest Code2406

--- src/internrocket/Switchprofile.java
+++ src/internrocket/Switchprofile.java
@@ -15,24 +15,24 @@ public class Switchprofile {
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/span/button")).click();
 		//Thread.sleep(4000);
 		//driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/section/article/div/div[2]/div/div/div[1]/div[4]/div/div/div/div/div/form/input[2]")).sendKeys("satti07");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/section/article/div/div[2]/div/div/div[1]/div[3]/div/div/div/div/div/form/input[1]")).sendKeys("satya455@yopmail.com");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/section/article/div/div[2]/div/div/div[1]/div[3]/div/div/div/div/div/form/input[2]")).sendKeys("satya2019");
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/section/article/div/div[2]/div/div/div[1]/div[3]/div/div/div/div/div/form/button")).click();
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[2]/div")).click();
 		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[2]/ul/div/div/li[3]/a")).click();
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[2]/ul/div/div/li[3]")).click();
 		Thread.sleep(4000);
-		driver.findElement(By.id("name")).sendKeys("ASR4");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/section/article/div/div[2]/div/form/div/div/div[1]/div[4]/div[2]/button[1]")).click();
-		Thread.sleep(6000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[2]/div")).click();
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[2]/ul/div/div/li[5]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[3]/div")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/header/div/div/div/div[2]/div/div[3]/ul/div/div/li[7]/a")).click();
 		
 		
 		
